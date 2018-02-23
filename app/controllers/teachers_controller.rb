f04cacac8471d8111c270c1cad3f6e62510310f0
Nhi Quach <nhiquach@berkeley.edu>
2018-02-22 23:58:43 -0800

    minor edits

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -5,6 +5,15 @@ class TeachersController < ApplicationController
 
   def create
     # Hint: params??
+
+    # YOUR CODE HERE
+
+
+    # This line is explicitly declaring that this controller method should render/serve the
+    # 'views/teachers/show.html.erb' view. Otherwise, the controller would try to look for a 
+    # 'views/teachers/create.html.erb' view file which we do not have. 
+    # By convention, create is associated with a POST/PUT method while show is associated with the GET HTTP method
+    # so the result after a "create" (C in CRUD) is a show.
     render 'show'
   end
 end
